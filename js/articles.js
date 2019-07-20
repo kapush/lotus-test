2819c9f9b1fcb6a64324269e524983bec50932d9
Kapush <kapushoniha@gmail.com>
2019-07-20 14:05:42 +0300

    articles adoptive

--- js/articles.js
+++ js/articles.js
@@ -14,8 +14,11 @@ renderArticles = (articles) => {
     const container = document.getElementById('articles');
     container.innerHTML = articles.map(article => `
     <li class="articles-item">
-        <h3>${article.id} ${article.title}</h3>
-        <div>${article.body}</div>
+        <div class="articles-item__container">
+            <h3>${article.id} ${article.title}</h3>
+            <div>${article.body}</div>
+        </div>
+        <a class="arrow" href="/features">Learn more <div class="arrow-right inline"></div></a>
     </li>
         `
     ).join('');
